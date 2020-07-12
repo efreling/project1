ff383ee0e6386120490da9191e6a3134bcb493a5
KarmaPanda <desmondlai30@gmail.com>
2020-07-12 17:33:02 -0400

    Updated content to include list.

--- desmond.php
+++ desmond.php
@@ -19,10 +19,48 @@ include "inc/html-top.php";
 		<div class="container">
 		<article>
 			<h2>Computer Science</h2>
-            <p>Before coming to the University of Rochester, I have taught myself programming languages using online resources. Eventually, during my senior year, I decided to take a formal course in Computer Science by taking AP Computer Science A where I learned to use Java, and competed in several hackathons. Through my participation in these hackathons, I realized that I can use my computer science abilities to create a better future, and I ended up majoring Computer Science to improve my knowledge in this field. Currently, I am familiar with C#, Python, Java, Kotlin, C, and Javascript.</p>
+            <p>Before coming to the University of Rochester, I have taught myself programming languages using online resources. Eventually, during my senior year, I decided to take a formal course in Computer Science by taking AP Computer Science A where I learned to use Java, and competed in several hackathons. Through my participation in these hackathons, I realized that I can use my computer science abilities to create a better future, and I ended up majoring Computer Science to improve my knowledge in this field.</p>
+
+            <h2>Classes in Computer Science</h2>
+            <ul>
+                <li>CSC 170: Intro to Web Development</li>
+                <li>CSC 172: Data Structures & Algorithms</li>
+                <li>CSC 173: Computation & Formal Systems</li>
+                <li>CSC 186: Video Game Development</li>
+                <li>CSC 210: Web Progamming</li>
+                <li>CSC 214: Mobile App Development</li>
+                <li>CSC 273W: Writing For Computer Science</li>
+            </ul>
+
+            <h2>Programming Languages</h2>
+            <ul>
+                <li>C#</li>
+                <li>Python</li>
+                <li>Java</li>
+                <li>Kotlin</li>
+                <li>C</li>
+                <li>Javascript</li>
+            </ul>
 
 			<h2>Digital Media Studies</h2>
 			<p>As I knew how to use certain tools such as Adobe Photoshop and Premiere before taking any Digital Media courses, I decided to improve upon these abilities when I came to college. After taking several courses in Digital Media improve my abilities with these tools, and I found that these skills apply towards many other fields. </p>
+
+            <h2>Classes in Digital Media Studies</h2>
+            <ul>
+                <li>DMS 103: Essential Digital Media Toolkit</li>
+                <li>DMS 111 / FMS 205 / SA 151: New Media & Emerging Practices</li>
+                <li>DMS 112 / SA 141: Introduction to Photography</li>
+            </ul>
+
+            <h2>Experienced in</h2>
+            <ul>
+                <li>Adobe Photoshop</li>
+                <li>Adobe Premiere</li>
+                <li>Adobe Illustrator</li>
+                <li>Audacity</li>
+                <li>Blender</li>
+                <li>Reality Capture</li>
+            </ul>
 
             <h2>SASE</h2>
             <p>Currently, I am the Fundraising Chair of the University of Rochester Society of Asian Scientists and Engineers. As the Fundraising Chair, I am responsible for organizing fundraisers through communicating with various offices and businesses to create a successful fundraiser. My role as the Fundraising Chair is important to the club because it is the primary means of funding the club's events.</p>
